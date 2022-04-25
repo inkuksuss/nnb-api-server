2b48bf83b34bebe11df88839d057182c57df3bb1
iot_김인국 <inkuksuss1@gmail.com>
2022-04-25 19:20:41 +0900

    한글 테스트

--- src/main/java/com/smartFarm/was/web/service/impl/CommentServiceImpl.java
+++ src/main/java/com/smartFarm/was/web/service/impl/CommentServiceImpl.java
@@ -89,7 +89,6 @@ public class CommentServiceImpl implements CommentService {
     @Override
     public ResultResponse<List<CommentDto>> deleteComment(DeleteCommentDto deleteCommentDto) throws SQLException {
 
-
         boolean isOwnerFlag = deleteCommentDto.isOwnerFlag();
         int result = commentRepository.deleteComment(deleteCommentDto);
 
